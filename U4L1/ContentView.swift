68c89cf09352f88740067a21b084d11213257033
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-10-30 17:04:24 +0300

    final proj

--- U4L1/ContentView.swift
+++ U4L1/ContentView.swift
@@ -9,16 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+            ZStack(alignment: .bottom){
+                Image(.img1)
+                    .resizable()
+                    .scaledToFit()
+                    .fixedSize()
+                    .foregroundStyle(.tint)
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod")
+                    .foregroundStyle(.white)
+                    .padding()
+                
+            }
+            HStack{
+                Image(.img2)
+                    .resizable()
+                    .scaledToFit()
+                  
+                Image(.img3)
+                    .resizable()
+                    .scaledToFit()
+                    
+                Image(.img4)
+                    .resizable()
+                    .scaledToFit()
+                    
+            }
+            .padding(5)
+            
+            HStack (){
+                HStack {
+                    Image(.img5)
+                    Text("User Name")
+                }
+                HStack {
+                    Image(.img6)
+                    Text("25")
+                }
+                Spacer()
+            }
+            .padding(10)
+            
+            
+            VStack (alignment: .leading){
+                Text("About")
+                Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod")
+            }
+            Spacer()
+            Button {
+            } label: {
+                Text("настроить профиль")
+                    .tint(.black)
+            }
         }
-        .padding()
+        
     }
 }
-
+    
 #Preview {
     ContentView()
 }
